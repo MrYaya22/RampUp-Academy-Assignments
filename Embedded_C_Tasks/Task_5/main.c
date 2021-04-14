a1a285047ed1d9b4d35edf75a057e1234bbabccf
MrYaya22 <53049314+MrYaya22@users.noreply.github.com>
2021-04-14 22:12:46 +0200

    Create main.c

---
+++ Embedded_C_Tasks/Task_5/main.c
@@ -0,0 +1,37 @@
+
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+typedef volatile int8_t     vint8_t ;
+typedef volatile uint8_t    vuint8_t ;
+typedef volatile int16_t     vint16_t ;
+typedef volatile uint16_t    vuint16_t ;
+typedef volatile int32_t     vint32_t ;
+typedef volatile uint32_t    vuint32_t ;
+typedef volatile int64_t     vint64_t ;
+typedef volatile uint64_t    vuint64_t ;
+
+typedef float   float32 ;
+typedef double  float64 ;
+typedef long double float96 ;
+typedef unsigned char           uint8 ;
+typedef signed char             sint8 ;
+typedef  unsigned short int     uint16 ;
+typedef  signed short int       sint16 ;
+typedef  unsigned long int      uint32 ;
+typedef  signed long int        sint32 ;
+typedef  unsigned long long int uint64 ;
+typedef  signed long long int   dsint64 ;
+
+typedef float   float32 ;
+typedef double  float64 ;
+typedef long double float96 ;
+
+
+
+#endif
+
