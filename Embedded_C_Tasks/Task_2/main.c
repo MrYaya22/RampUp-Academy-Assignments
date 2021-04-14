9cb7d0138cc74cd61a34e1d47cffa7e2e755dde8
MrYaya22 <53049314+MrYaya22@users.noreply.github.com>
2021-04-14 22:09:41 +0200

    Create main.c

---
+++ Embedded_C_Tasks/Task_2/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "swap_Fun_address.h"
+
+
+
+int main()
+{
+   int x, y;
+
+   printf("Enter the value of x and y\n");
+   scanf("%d%d",&x,&y);
+
+   printf("Before Swapping\nx = %d\ny = %d\n", x, y);
+
+   swap(&x, &y);
+
+   printf("Main Function values\n\nx = %d\ny = %d\n", x, y);
+
+   return 0;
+}
+
